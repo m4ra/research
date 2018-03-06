77119a45d2ee5454dff8ebbea86994980799d673
Mara Karagianni <mara@multiplace.org>
2018-03-06 20:24:42 +0200

    new models

--- phd/admin.py
+++ phd/admin.py
@@ -1,27 +1,32 @@
+from django.utils.html import format_html
+from django.urls import reverse
+
 from django.contrib import admin
 from phd.models import *
 
 
 class MaterialAdmin(admin.ModelAdmin):
-        #fields = _all_
-        pass
+	#fields = _all_
+	list_display = ('title', 'kind', 'year')
+	pass
 
 
 class FieldAdmin(admin.ModelAdmin):
-        #fields = _all_
-        pass
+	#fields = _all_
+	list_display = ('name', 'related_materials',)
+	pass
 
 class ConceptAdmin(admin.ModelAdmin):
-        #fields = _all_
-        pass
+	#fields = _all_
+	pass
 
 class NoteAdmin(admin.ModelAdmin):
-        #fields = _all_
-        pass
+	#fields = _all_
+	pass
 
 class ChapterAdmin(admin.ModelAdmin):
-        #fields = _all_
-        pass
+	#fields = _all_
+	pass
 
 admin.site.register(Material, MaterialAdmin)
 admin.site.register(Chapter, ChapterAdmin)
